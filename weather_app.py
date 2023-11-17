04f12a430b31aa2b7c71beab14a376d6dc3e909a
Shaun <79271767+shaunck96@users.noreply.github.com>
2023-11-17 12:05:22 -0500

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -12,100 +12,6 @@ from bs4 import BeautifulSoup
 from PIL import Image
 import psycopg2
 
-try:
-    connection = psycopg2.connect(host="localhost", port="5434", database="master",
-                                user="postgres", password="Toyotacamry1996#")
-    crsr = connection.cursor()
-
-    print('PostgreSQL database version: ')
-    crsr.execute('SELECT version()')
-    db_version = crsr.fetchone()
-    print(db_version)
-
-except:
-    print("Connection Error")
-
-##HISTORIC DATA
-crsr.execute("Select * FROM historic_weather LIMIT 0")
-colnames = [desc[0] for desc in crsr.description]
-crsr.execute("""SELECT *
-FROM historic_weather
-WHERE upload_timestamp = (
-    SELECT MAX(upload_timestamp)
-    FROM historic_weather)""")
-
-weather_data = crsr.fetchall()
-
-if weather_data:
-    columns = colnames  # Replace with your actual column names
-    historic_weather_df = pd.DataFrame(weather_data, columns=columns)
-    historic_weather_df.to_csv(r'C:\Users\307164\Desktop\Weather Chat Bot\historic_weather_data.csv')
-    print(historic_weather_df)
-else:
-    print("No data fetched")
-
-##CURRENT WEATHER
-crsr.execute("Select * FROM current_weather LIMIT 0")
-colnames = [desc[0] for desc in crsr.description]
-crsr.execute("""SELECT *
-FROM current_weather
-WHERE upload_timestamp = (
-    SELECT MAX(upload_timestamp)
-    FROM current_weather)""")
-
-current_weather = crsr.fetchall()
-
-if current_weather:
-    columns = colnames  # Replace with your actual column names
-    current_weather_df = pd.DataFrame(current_weather, columns=columns)
-    current_weather_df.to_csv(r'C:\Users\307164\Desktop\Weather Chat Bot\current_weather.csv')
-    print(current_weather_df)
-else:
-    print("No data fetched")
-
-
-##NWS WEATHER
-crsr.execute("Select * FROM nws_weather_forecast LIMIT 0")
-colnames = [desc[0] for desc in crsr.description]
-crsr.execute("""SELECT *
-FROM nws_weather_forecast
-WHERE upload_timestamp = (
-    SELECT MAX(upload_timestamp)
-    FROM nws_weather_forecast)""")
-
-forecast_df_nws = crsr.fetchall()
-
-if forecast_df_nws:
-    columns = colnames  # Replace with your actual column names
-    nws_weather_forecast_df = pd.DataFrame(forecast_df_nws, columns=columns)
-    nws_weather_forecast_df.to_csv(r'C:\Users\307164\Desktop\Weather Chat Bot\nws_weather_forecast.csv')
-    print(nws_weather_forecast_df)
-else:
-    print("No data fetched")
-
-
-##NWS WEATHER
-crsr.execute("Select * FROM time_and_day_weather_forecast LIMIT 0")
-colnames = [desc[0] for desc in crsr.description]
-crsr.execute("""SELECT *
-FROM time_and_day_weather_forecast
-WHERE upload_timestamp = (
-    SELECT MAX(upload_timestamp)
-    FROM time_and_day_weather_forecast)""")
-
-time_and_day_weather_forecast = crsr.fetchall()
-
-if time_and_day_weather_forecast:
-    columns = colnames  # Replace with your actual column names
-    time_and_day_weather_forecast_df = pd.DataFrame(time_and_day_weather_forecast, columns=columns)
-    time_and_day_weather_forecast_df.to_csv(r'C:\Users\307164\Desktop\Weather Chat Bot\time_and_day_weather_forecast.csv')
-    print(time_and_day_weather_forecast_df)
-else:
-    print("No data fetched")
-
-
-connection.close()
-
 image_url = "https://www.spc.noaa.gov/public/state/images/RI_swody1.png"
 
 # Send a GET request to the URL
@@ -114,7 +20,7 @@ response = requests.get(image_url)
 # Check if the request was successful
 if response.status_code == 200:
     # Open a file in binary write mode
-    with open(r"C:\Users\307164\Desktop\Weather Chat Bot\RI_swody1.png", 'wb') as file:
+    with open(r"./RI_swody1.png", 'wb') as file:
         # Write the content of the response to the file
         file.write(response.content)
     print("Image downloaded successfully.")
@@ -129,7 +35,7 @@ response = requests.get(image_url)
 # Check if the request was successful
 if response.status_code == 200:
     # Open a file in binary write mode
-    with open(r"C:\Users\307164\Desktop\Weather Chat Bot\RI_swody2.png", 'wb') as file:
+    with open(r"./RI_swody2.png", 'wb') as file:
         # Write the content of the response to the file
         file.write(response.content)
     print("Image downloaded successfully.")
@@ -144,13 +50,14 @@ response = requests.get(image_url)
 # Check if the request was successful
 if response.status_code == 200:
     # Open a file in binary write mode
-    with open(r"C:\Users\307164\Desktop\Weather Chat Bot\RI_swody3.png", 'wb') as file:
+    with open(r"./RI_swody3.png", 'wb') as file:
         # Write the content of the response to the file
         file.write(response.content)
     print("Image downloaded successfully.")
 else:
     print(f"Failed to retrieve image. Status code: {response.status_code}")
 
+weather_data = pd.read_csv(r'./historic_weather_data.csv')
 # Define function to filter data based on date range
 def filter_data(start_date, end_date):
     start_date = pd.to_datetime(start_date)
@@ -219,12 +126,14 @@ current_weather_info = f"Temperature: {temp} °C\nTime: {time}\nSky Description:
 
 st.markdown(f"```{current_weather_info}```", unsafe_allow_html=True)
 st.subheader("Storm Outlook for Today")
-img1 = Image.open(r'C:\Users\307164\Desktop\Weather Chat Bot\RI_swody1.png')
+img1 = Image.open(r'./RI_swody1.png')
 st.image([img1], width=600, caption=["Image 1"], use_column_width=True)
 
 # Display forecasts
 st.subheader("Weather Forecasts")
 
+nws_weather_forecast_df = pd.read_csv(r'./nws_weather_forecast.csv')
+nws_weather_forecast_df = pd.read_csv(r'./time_and_day_weather_forecast.csv')
 # Display forecasts from CSV files as stylish tables with headers
 print(forecast_df_nws)
 st.dataframe(nws_weather_forecast_df.style
@@ -238,7 +147,7 @@ st.dataframe(time_and_day_weather_forecast_df.style
 # Display images more prominently and larger
 st.subheader("Severe Weather Outlook for Next 2 Days")
 
-img2 = Image.open(r'C:\Users\307164\Desktop\Weather Chat Bot\RI_swody2.png')
-img3 = Image.open(r'C:\Users\307164\Desktop\Weather Chat Bot\RI_swody3.png')
+img2 = Image.open(r'./RI_swody2.png')
+img3 = Image.open(r'./RI_swody3.png')
 
 st.image([img2, img3], width=600, caption=["Image 2", "Image 3"], use_column_width=True)
