ad7d93034f7e3ffaacb0fbb945c390bed405a61d
Shaun <79271767+shaunck96@users.noreply.github.com>
2023-11-22 10:14:27 -0500

    Update weather_app_updated.py

--- weather_app_updated.py
+++ weather_app_updated.py
@@ -192,14 +192,115 @@ hourly_data["soil_moisture_9_to_27cm"] = hourly_soil_moisture_9_to_27cm
 hourly_data["soil_moisture_27_to_81cm"] = hourly_soil_moisture_27_to_81cm
 
 hourly_dataframe = pd.DataFrame(data = hourly_data)
-hourly_dataframe.head(5)
 
-weather_data = pd.read_csv(r'./historic_weather_data.csv')
+# Get the current date
+current_date = datetime.now()
+# Subtract 5 days from the current date
+new_date = current_date - timedelta(days=5)
+# Format the new date as a string in "YYYY-MM-DD" format
+end_date = new_date.strftime("%Y-%m-%d")
+
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after = -1)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
+
+# Make sure all required weather variables are listed here
+# The order of variables in hourly or daily is important to assign them correctly below
+url = "https://archive-api.open-meteo.com/v1/archive"
+params = {
+	"latitude": 41.824,
+	"longitude": -71.418884,
+	"start_date": "2018-01-01",
+	"end_date": end_date,
+	"hourly": ["temperature_2m", "relative_humidity_2m", "dew_point_2m", "apparent_temperature", "precipitation", "rain", "snowfall", "snow_depth", "weather_code", "pressure_msl", "surface_pressure", "cloud_cover", "cloud_cover_low", "cloud_cover_mid", "cloud_cover_high", "et0_fao_evapotranspiration", "vapour_pressure_deficit", "wind_speed_10m", "wind_speed_100m", "wind_direction_10m", "wind_direction_100m", "wind_gusts_10m", "soil_temperature_0_to_7cm", "soil_temperature_7_to_28cm", "soil_temperature_28_to_100cm", "soil_temperature_100_to_255cm", "soil_moisture_0_to_7cm", "soil_moisture_7_to_28cm", "soil_moisture_28_to_100cm", "soil_moisture_100_to_255cm"]
+}
+responses = openmeteo.weather_api(url, params=params)
+
+# Process first location. Add a for-loop for multiple locations or weather models
+response = responses[0]
+print(f"Coordinates {response.Latitude()}°E {response.Longitude()}°N")
+print(f"Elevation {response.Elevation()} m asl")
+print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+
+# Process hourly data. The order of variables needs to be the same as requested.
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+hourly_dew_point_2m = hourly.Variables(2).ValuesAsNumpy()
+hourly_apparent_temperature = hourly.Variables(3).ValuesAsNumpy()
+hourly_precipitation = hourly.Variables(4).ValuesAsNumpy()
+hourly_rain = hourly.Variables(5).ValuesAsNumpy()
+hourly_snowfall = hourly.Variables(6).ValuesAsNumpy()
+hourly_snow_depth = hourly.Variables(7).ValuesAsNumpy()
+hourly_weather_code = hourly.Variables(8).ValuesAsNumpy()
+hourly_pressure_msl = hourly.Variables(9).ValuesAsNumpy()
+hourly_surface_pressure = hourly.Variables(10).ValuesAsNumpy()
+hourly_cloud_cover = hourly.Variables(11).ValuesAsNumpy()
+hourly_cloud_cover_low = hourly.Variables(12).ValuesAsNumpy()
+hourly_cloud_cover_mid = hourly.Variables(13).ValuesAsNumpy()
+hourly_cloud_cover_high = hourly.Variables(14).ValuesAsNumpy()
+hourly_et0_fao_evapotranspiration = hourly.Variables(15).ValuesAsNumpy()
+hourly_vapour_pressure_deficit = hourly.Variables(16).ValuesAsNumpy()
+hourly_wind_speed_10m = hourly.Variables(17).ValuesAsNumpy()
+hourly_wind_speed_100m = hourly.Variables(18).ValuesAsNumpy()
+hourly_wind_direction_10m = hourly.Variables(19).ValuesAsNumpy()
+hourly_wind_direction_100m = hourly.Variables(20).ValuesAsNumpy()
+hourly_wind_gusts_10m = hourly.Variables(21).ValuesAsNumpy()
+hourly_soil_temperature_0_to_7cm = hourly.Variables(22).ValuesAsNumpy()
+hourly_soil_temperature_7_to_28cm = hourly.Variables(23).ValuesAsNumpy()
+hourly_soil_temperature_28_to_100cm = hourly.Variables(24).ValuesAsNumpy()
+hourly_soil_temperature_100_to_255cm = hourly.Variables(25).ValuesAsNumpy()
+hourly_soil_moisture_0_to_7cm = hourly.Variables(26).ValuesAsNumpy()
+hourly_soil_moisture_7_to_28cm = hourly.Variables(27).ValuesAsNumpy()
+hourly_soil_moisture_28_to_100cm = hourly.Variables(28).ValuesAsNumpy()
+hourly_soil_moisture_100_to_255cm = hourly.Variables(29).ValuesAsNumpy()
+
+hourly_historic_data = {"date": pd.date_range(
+	start = pd.to_datetime(hourly.Time(), unit = "s"),
+	end = pd.to_datetime(hourly.TimeEnd(), unit = "s"),
+	freq = pd.Timedelta(seconds = hourly.Interval()),
+	inclusive = "left"
+)}
+hourly_historic_data["temperature_2m"] = hourly_temperature_2m
+hourly_historic_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+hourly_historic_data["dew_point_2m"] = hourly_dew_point_2m
+hourly_historic_data["apparent_temperature"] = hourly_apparent_temperature
+hourly_historic_data["precipitation"] = hourly_precipitation
+hourly_historic_data["rain"] = hourly_rain
+hourly_historic_data["snowfall"] = hourly_snowfall
+hourly_historic_data["snow_depth"] = hourly_snow_depth
+hourly_historic_data["weather_code"] = hourly_weather_code
+hourly_historic_data["pressure_msl"] = hourly_pressure_msl
+hourly_historic_data["surface_pressure"] = hourly_surface_pressure
+hourly_historic_data["cloud_cover"] = hourly_cloud_cover
+hourly_historic_data["cloud_cover_low"] = hourly_cloud_cover_low
+hourly_historic_data["cloud_cover_mid"] = hourly_cloud_cover_mid
+hourly_historic_data["cloud_cover_high"] = hourly_cloud_cover_high
+hourly_historic_data["et0_fao_evapotranspiration"] = hourly_et0_fao_evapotranspiration
+hourly_historic_data["vapour_pressure_deficit"] = hourly_vapour_pressure_deficit
+hourly_historic_data["wind_speed_10m"] = hourly_wind_speed_10m
+hourly_historic_data["wind_speed_100m"] = hourly_wind_speed_100m
+hourly_historic_data["wind_direction_10m"] = hourly_wind_direction_10m
+hourly_historic_data["wind_direction_100m"] = hourly_wind_direction_100m
+hourly_historic_data["wind_gusts_10m"] = hourly_wind_gusts_10m
+hourly_historic_data["soil_temperature_0_to_7cm"] = hourly_soil_temperature_0_to_7cm
+hourly_historic_data["soil_temperature_7_to_28cm"] = hourly_soil_temperature_7_to_28cm
+hourly_historic_data["soil_temperature_28_to_100cm"] = hourly_soil_temperature_28_to_100cm
+hourly_historic_data["soil_temperature_100_to_255cm"] = hourly_soil_temperature_100_to_255cm
+hourly_historic_data["soil_moisture_0_to_7cm"] = hourly_soil_moisture_0_to_7cm
+hourly_historic_data["soil_moisture_7_to_28cm"] = hourly_soil_moisture_7_to_28cm
+hourly_historic_data["soil_moisture_28_to_100cm"] = hourly_soil_moisture_28_to_100cm
+hourly_historic_data["soil_moisture_100_to_255cm"] = hourly_soil_moisture_100_to_255cm
+
+weather_data = pd.DataFrame(data = hourly_historic_data)
+
 # Define function to filter data based on date range
 def filter_data(start_date, end_date):
     start_date = pd.to_datetime(start_date)
     end_date = pd.to_datetime(end_date)
-    filtered_data = weather_data[(pd.to_datetime(weather_data['local_time']) >= start_date) & (pd.to_datetime(weather_data['local_time']) <= end_date)]
+    filtered_data = weather_data[(pd.to_datetime(weather_data['date']) >= start_date) & (pd.to_datetime(weather_data['date']) <= end_date)]
     return filtered_data
 
 # Streamlit app layout
@@ -215,25 +316,38 @@ if st.button("Generate Plots"):
         filtered_data = filter_data(start_date, end_date)
 
         # Weather metrics to plot
-        weather_metrics = ['t2m', 'prectotland', 'precsnoland', 'snomas', 'rhoa', 'swgdn', 'swtdn', 'cldtot']
+        weather_metrics = ['temperature_2m', 'relative_humidity_2m', 'dew_point_2m',
+       'apparent_temperature', 'precipitation', 'rain', 'snowfall',
+       'snow_depth', 'weather_code', 'pressure_msl', 'surface_pressure',
+       'cloud_cover', 'cloud_cover_low', 'cloud_cover_mid', 'cloud_cover_high',
+       'et0_fao_evapotranspiration', 'vapour_pressure_deficit',
+       'wind_speed_10m', 'wind_speed_100m', 'wind_direction_10m',
+       'wind_direction_100m', 'wind_gusts_10m', 'soil_temperature_0_to_7cm',
+       'soil_temperature_7_to_28cm', 'soil_temperature_28_to_100cm',
+       'soil_temperature_100_to_255cm', 'soil_moisture_0_to_7cm',
+       'soil_moisture_7_to_28cm', 'soil_moisture_28_to_100cm',
+       'soil_moisture_100_to_255cm']
 
         # Create subplots
         fig, axes = plt.subplots(nrows=len(weather_metrics), ncols=1, figsize=(10, 6 * len(weather_metrics)))
         fig.suptitle('Weather Metrics Over Time', fontsize=16)
-        colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'purple']
+        colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'purple', 'orange', 'brown',
+                 'pink', 'gray', 'olive', 'teal', 'navy', 'indigo', 'cyan', 'violet',
+                 'lime', 'gold', 'silver', 'maroon', 'darkgreen', 'darkblue', 'darkred',
+                 'darkorange', 'darkcyan', 'darkviolet']
 
         for i, (metric, color) in enumerate(zip(weather_metrics, colors)):
             ax = axes[i]
-            ax.plot(filtered_data['local_time'], filtered_data[metric], color=color, marker='o', markersize=5, label=metric)
+            ax.plot(filtered_data['date'], filtered_data[metric], color=color, marker='o', markersize=5, label=metric)
             ax.set_ylabel(metric)
             ax.grid(True)
             ax.set_title(metric)
             ax.legend()
 
             # Add marker labels (values) at specific data points
-            for index, value in filtered_data[[metric, 'local_time']].iterrows():
-                ax.text(value['local_time'], value[metric], round(float(value[metric]), 0), ha='left', va='bottom', fontsize=6)
-
+            for index, value in filtered_data[[metric, 'date']].iterrows():
+                ax.text(value['date'], value[metric], round(int(value[metric]), 0), ha='left', va='bottom', fontsize=6)
+		    
         # Customize plot appearance
         plt.xlabel('Date')
         plt.xticks(rotation=45)
