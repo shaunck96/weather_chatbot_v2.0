8df7b9dc7d67758736781b89985656f9fc7aff6a
Shaun <79271767+shaunck96@users.noreply.github.com>
2023-11-22 10:11:31 -0500

    Update weather_app_latest.py

--- weather_app_latest.py
+++ weather_app_latest.py
@@ -346,11 +346,7 @@ if st.button("Generate Plots"):
 
             # Add marker labels (values) at specific data points
             for index, value in filtered_data[[metric, 'date']].iterrows():
-                if float(value[metric]):
-                    float_value = float(value[metric])
-                else:
-                    float_value = 0.0  # or any other default value
-                ax.text(value['date'], float_value, round(float_value, 0), ha='left', va='bottom', fontsize=6)
+                ax.text(value['date'], value[metric], round(int(value[metric]), 0), ha='left', va='bottom', fontsize=6)
 		    
         # Customize plot appearance
         plt.xlabel('Date')
